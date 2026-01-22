99f5e4ae97411f46386cccb5ccb5c55eb1176899
Luis Ramiro Krupoviesa <Ramiro>
2026-01-22 18:34:46 -0300

    improve button component

--- src/game/components/UIComponents.ts
+++ src/game/components/UIComponents.ts
@@ -10,24 +10,27 @@ export class Button extends GameObjects.Container {
     super(scene, x, y);
 
     const textColor = '#ffffff'
-    const textButton = this.scene.add.text(0, 0, props.text, {
+    const textButton = new GameObjects.Text(scene, 0, 0, props.text, {
       backgroundColor: '#333',
-      padding: { x: 20, y: 10 },
+      padding: {
+        x: 20,
+        y: 10
+      },
       color: textColor
     })
 
     this.x = this.x - textButton.width / 2
     this.y = this.y - textButton.height / 2
 
-    this.scene.add.existing(this)
     const containerArea = new Geom.Rectangle(0, 0, textButton.width, textButton.height)
     this.setInteractive(containerArea, Geom.Rectangle.Contains)
 
-    this.add([textButton])
+    this.add(textButton)
 
     this
       .on('pointerover', () => textButton.setStyle({ fill: '#ff0' }))
       .on('pointerout', () => textButton.setStyle({ fill: textColor }))
       .on('pointerdown', props.onClick)
+    this.scene.add.existing(this)
   }
 }
