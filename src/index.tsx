fe001b5ebccf030a8fc4d9be36ce12b3a9e79c68
Luis Ramiro Krupoviesa <Ramiro>
2026-01-23 18:46:59 -0300

    refactor solid UI

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import { render } from 'solid-js/web'
 import { Router, Route } from "@solidjs/router";
 
 import './index.css'
-import App from './App.tsx'
 import Debug from './debug.tsx';
 import Layout from './Layout.tsx';
+import { Index } from './pages/Index.tsx';
 
 const root = document.getElementById('root')
 
 render(() =>
   <Router root={Layout}>
-    <Route path="/" component={App} />
+    <Route path="/" component={Index} />
     <Route path="/debug" component={Debug} />
   </Router>, root!)
