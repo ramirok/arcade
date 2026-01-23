2884a6d13d791c9a73410d3a455d3bffdd0f2269
Luis Ramiro Krupoviesa <Ramiro>
2026-01-23 18:47:07 -0300

    improve types override

--- src/game/utils.ts
+++ src/game/utils.ts
@@ -46,10 +46,15 @@ export const isWithinRange = (x1: number, y1: number, x2: number, y2: number, ra
 //   return { data, events };
 // };
 
-export type DataOverride<Entity, Data> = Omit<Phaser.Data.DataManager, 'set' | 'get' | 'inc' | 'events'> & {
-  set: <T extends keyof Data>(dataKey: T, data: Data[T]) => Phaser.Data.DataManager
+export type DataOverride<Entity, Data> = Omit<Phaser.Data.DataManager, 'set' | 'get' | 'inc' | 'events' | 'toggle'> & {
+  // set: <T extends keyof Data>(dataKey: T, data: Data[T]) => Phaser.Data.DataManager
+  set: {
+    (dataObject: Partial<Data>): Phaser.Data.DataManager
+    <T extends keyof Data>(dataKey: T, data: Data[T]): Phaser.Data.DataManager
+  }
   get: <T extends keyof Data>(dataKey: T) => Data[T]
   inc: <T extends keyof Data>(dataKey: T, amount: number) => Phaser.Data.DataManager
+  toggle: <T extends keyof Data>(dataKey: T) => Phaser.Data.DataManager
   events: Omit<Phaser.Events.EventEmitter, 'on'> & {
     on: {
       <T extends keyof Data>(event: `changedata-${keyof Data & string}`, cb: (entity: Entity, newData: Data[T], oldData: Data[T]) => void): Phaser.Events.EventEmitter;
