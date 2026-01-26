3514309f383c6f2bcfe28fba879f4f410828d9af
Luis Ramiro Krupoviesa <Ramiro>
2026-01-25 21:47:12 -0300

    move bottom bar UI to BottomBarLayout

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,16 @@ import { Router, Route } from "@solidjs/router";
 
 import './index.css'
 import { IndexPage } from './pages/Index.tsx';
-import { DebugPage } from './pages/Debug.tsx';
 import { RootLayout } from './layouts/RootLayout.tsx';
 import { PlayPage } from './pages/Play.tsx';
+import { BottomBarLayout } from './layouts/BottomBarLayout.tsx';
 
 const root = document.getElementById('root')
 
 render(() =>
   <Router root={RootLayout}>
     <Route path="/" component={IndexPage} />
-    <Route path="/play" component={PlayPage} />
-    <Route path="/debug" component={DebugPage} />
+    <Route path='/play' component={BottomBarLayout}>
+      <Route path="/" component={PlayPage} />
+    </Route>
   </Router>, root!)
