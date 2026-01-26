e86bcd3b577185cd202977bf64eeccea37d5ce71
Luis Ramiro Krupoviesa <Ramiro>
2026-01-25 21:34:48 -0300

    remove unused code in IndexPage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,36 +1,4 @@
-import { createStore } from "solid-js/store";
-import { useGame } from "../Layout";
-import type { MainGame } from "../game/scenes/MainGame";
-import { CharStats } from "../components/CharStats";
-import { Show } from "solid-js";
-
 export const IndexPage = () => {
-
-  const gameInstance = useGame()
-
-  const mainGameScene = gameInstance().scene.getScene('main-game') as MainGame
-  const player = mainGameScene.player
-
-  const [store, setStore] = createStore({
-    health: player.data.get('health'),
-    attributeMaxHealth: player.data.get('attributeMaxHealth'),
-    xp: player.data.get('xp'),
-    xpToNextLVL: player.data.get('xpToNextLVL'),
-    mana: player.data.get('mana'),
-    attributeMaxMana: player.data.get('attributeMaxMana'),
-    chartStatsOpen: false
-  })
-
-
-
-  player.data.events.on('changedata', (_, dataKey, data) => {
-    if (dataKey in store) {
-      setStore(dataKey as keyof typeof store, data)
-    }
-  })
-  mainGameScene.data.events.on('changedata-charStatsOpen', (_, data) => {
-    setStore('chartStatsOpen', data)
-  })
 
   return (
     <>
